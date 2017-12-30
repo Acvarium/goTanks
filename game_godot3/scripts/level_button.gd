2495986db4926fe4ed2cea2c8bdf8faef2c48bde
Acvarium <vshmorgun@yandex.ru>
2017-12-30 02:24:00 +0200

    Added preview for levels to menu and removed builds

--- game_godot3/scripts/level_button.gd
+++ game_godot3/scripts/level_button.gd
@@ -1,9 +1,11 @@
 extends Node2D
 export var level = 0
+var main_node
 
 func _ready():
+	main_node = get_node("/root/main")
 	$Button.text = str(level)
-
+	
 func set_level(l):
 	level = l
 	$Button.text = str(level)
@@ -11,3 +13,11 @@ func set_level(l):
 func _on_Button_pressed():
 	global.level = level
 	global.goto_scene("res://scenes/main.tscn")
+
+
+func _on_Button_mouse_entered():
+	main_node.mouse_over(level)
+
+
+func _on_Button_mouse_exited():
+	main_node.mouse_exited()
